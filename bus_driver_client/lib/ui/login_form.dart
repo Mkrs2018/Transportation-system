aec5effc8bca3a4b9050ffa0a1dc8c6a72d0be0a
Mahesh Chandran <maheshchandran147@gmail.com>
2018-07-14 17:30:29 +0530

    Added appbar

---
+++ bus_driver_client/lib/ui/login_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade700,
+        title: new Text(
+          "Bus Driver's Portal"
+        )
+      ),
+    );
+  }
+}
